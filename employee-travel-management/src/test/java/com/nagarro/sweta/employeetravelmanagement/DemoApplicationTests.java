6132b067a78cf2a1339258715d21236f6ad1fb4d
Sweta Chaurasia <sweta.chaurasia@nagarro.com>
2020-04-25 05:32:42 +0530

    init

---
+++ employee-travel-management/src/test/java/com/nagarro/sweta/employeetravelmanagement/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nagarro.sweta.employeetravelmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
